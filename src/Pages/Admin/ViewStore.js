c9fbc6519afff46c14f04535b700a8adf3c99f9e
Kim Smirnov <kim.smirnov@gmail.com>
2022-03-30 13:07:37 +0300

    commit

--- src/Pages/Admin/ViewStore.js
+++ src/Pages/Admin/ViewStore.js
@@ -3,80 +3,97 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function ViewStore() {
+function ViewLocation() {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const [store, updateProducts] = useState([]);
-    const [originalProducts, updateOriginalProducts] = useState([]);
-    const searchRef = useRef();
+    const [location, updateLocation] = useState([]);
+    const [timeslot, updateTimeslot] = useState([]);
+    // const [originalProducts, updateOriginalProducts] = useState([]);
+    // const searchRef = useRef();
 
     useEffect(() => {
-        fetch("https://webshop1234.herokuapp.com/store")
+        fetch("http://localhost:8080/location")
         .then(res => res.json())
         .then(body => {
-            updateProducts(body)
-            updateOriginalProducts(body);
+            updateLocation(body)
+            // updateOriginalProducts(body);
         });
     
         },[]);
 
-    function searchProduct() {
-        let productsFound = [];
-        console.log(originalProducts);
-        console.log(searchRef.current.value);
-        originalProducts.forEach(product => {
-            if (product.name.toUpperCase().indexOf(searchRef.current.value.toUpperCase()) > -1
-                || product.barcode.toString().indexOf(searchRef.current.value) > -1 ) {
-                productsFound.push(product);
-            }
-        })
-        updateProducts(productsFound);
-    }
+    useEffect(() => {
+            fetch("http://localhost:8080/timeslot")
+            .then(res => res.json())
+            .then(body => {
+                updateTimeslot(body)
+                // updateOriginalProducts(body);
+            });
+        
+            },[]);
 
-    function onDeleteProduct(product) {
-        fetch("https://webshop1234.herokuapp.com/store/" + product.id, {
-            method: "DELETE"
-        })
-        .then(res => res.json()) 
-        .then(data => {
-            updateProducts(data)
-            updateOriginalProducts(data);
-        });
-    }
+    // function searchProduct() {
+    //     let productsFound = [];
+    //     console.log(originalProducts);
+    //     console.log(searchRef.current.value);
+    //     originalProducts.forEach(product => {
+    //         if (product.name.toUpperCase().indexOf(searchRef.current.value.toUpperCase()) > -1
+    //             || product.barcode.toString().indexOf(searchRef.current.value) > -1 ) {
+    //             productsFound.push(product);
+    //         }
+    //     })
+    //     updateProducts(productsFound);
+    // }
+
+    // function onDeleteProduct(product) {
+    //     fetch("https://webshop1234.herokuapp.com/store/" + product.id, {
+    //         method: "DELETE"
+    //     })
+    //     .then(res => res.json()) 
+    //     .then(data => {
+    //         updateProducts(data)
+    //         updateOriginalProducts(data);
+    //     });
+    // }
 
     
     return (<div>
-        <h2 className="mb-4">store</h2>
-        <input onKeyUp={searchProduct} ref={searchRef} type="text" />
+        <h2 className="mb-4">location</h2>
+        {/* <input onKeyUp={searchProduct} ref={searchRef} type="text" /> */}
   <table className="table table-hover table-bordered">
     <thead>
     <tr>
+      <th scope="col">id</th>
       <th scope="col">Name</th>
-      <th scope="col">Aadress</th>
-      <th scope="col">openHours</th>
-      <th scope="col">Actions</th>
+      <th scope="col">Address</th>
+      <th scope="col">Open Time</th>
+      <th scope="col">Close Time</th>
+      <th scope="col">TimeSlots</th>
     </tr>
     </thead>
     <tbody>
-    {store.map(store => <tr>
-      <td>{store.name}</td>
-      <td>{store.address}</td>
-      <td>{store.openHours}</td>
+    {location.map(location => <tr>
+      <td>{location.id}</td>
+      <td>{location.name}</td>
+      <td>{location.longAddress}</td>
+      <td>{location.locationInitialTime}</td>
+      <td>{location.locationEndTime}</td>
+      <td>{timeslot.filter(element => element.location.id === location.timeSlots.id).map(timeSlots => <tr>{timeSlots}</tr>)
+      }</td>
       <td>
-          <Button onClick={() => onDeleteProduct(store)} variant="danger">X</Button>
+          {/* <Button onClick={() => onDeleteProduct(store)} variant="danger">X</Button>
           <Link to={"/admin/muudaStore/" + store.id}>
             <Button variant="warning">Edit</Button>
-          </Link>
+          </Link> */}
       </td>
     </tr>)}
 
     
     </tbody>
   </table>
-        <Modal show={show} onHide={handleClose}>
+        {/* <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
             <Modal.Title>Hoiatus</Modal.Title>
             </Modal.Header>
@@ -91,10 +108,10 @@ function ViewStore() {
             </Modal.Footer>
         </Modal>
         <br />
-        <Button variant="danger" onClick={handleShow}>Kustuta kõik tooted andmebaasist</Button>
+        <Button variant="danger" onClick={handleShow}>Kustuta kõik tooted andmebaasist</Button> */}
         <br /><br />
         
     </div>)
 }
 
-export default ViewStore;
+export default ViewLocation;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,41 +10,47 @@ function Home() {
     const { t } = useTranslation();
     const [products, updateProducts] = useState([]);
 
-    useEffect(() => {
-    fetch("https://webshop1234.herokuapp.com/products")
-    .then(res => res.json())
-    .then(body => updateProducts(body));
-    },[]);
+
+
+
+
+
+
+    // useEffect(() => {
+    // fetch("https://webshop1234.herokuapp.com/products")
+    // .then(res => res.json())
+    // .then(body => updateProducts(body));
+    // },[]);
     
-    function sortNameAsc() {
-        products.sort((a, b) => a.name.localeCompare(b.name));
-        updateProducts(products.slice());
-    }
+    // function sortNameAsc() {
+    //     products.sort((a, b) => a.name.localeCompare(b.name));
+    //     updateProducts(products.slice());
+    // }
 
-    function sortNameDesc() {
-        products.sort((a, b) => b.name.localeCompare(a.name));
-        updateProducts(products.slice());
-    }
+    // function sortNameDesc() {
+    //     products.sort((a, b) => b.name.localeCompare(a.name));
+    //     updateProducts(products.slice());
+    // }
 
-    function sortPriceAsc() {
-        products.sort((a, b) => a.price - b.price);
-        updateProducts(products.slice());
-    }
+    // function sortPriceAsc() {
+    //     products.sort((a, b) => a.price - b.price);
+    //     updateProducts(products.slice());
+    // }
 
-    function sortPriceDesc() {
-        products.sort((a, b) => b.price - a.price);
-        updateProducts(products.slice());
-    }
+    // function sortPriceDesc() {
+    //     products.sort((a, b) => b.price - a.price);
+    //     updateProducts(products.slice());
+    // }
 
     return (
     <div>
         <div className="App">
         </div> <br/><br/>
         {products.length > 0 && <div>
-        <Button onClick={sortNameAsc}>{t("home.sort-az")}</Button>
+        {/* <Button onClick={sortNameAsc}>{t("home.sort-az")}</Button>
         <Button onClick={sortNameDesc}>{t("home.sort-za")}</Button>
         <Button onClick={sortPriceAsc}>{t("home.sort-price-asc")}</Button>
-        <Button onClick={sortPriceDesc}>{t("home.sort-price-desc")}</Button>
+        <Button onClick={sortPriceDesc}>{t("home.sort-price-desc")}</Button> */}
         </div>}
         {products.map(product => {return product.quantity > 0 && <Product key={product.id} prod={product} />})}
     </div>)
